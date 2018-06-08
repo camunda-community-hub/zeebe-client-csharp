d77c117fe71a402456c6df259e598c507c81f7a6
Christopher Zell <Zelldon91@gmail.com>
2018-06-08 08:54:37 +0200

    add client project

---
+++ zb-client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zbclient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MainClass m = new MainClass();
+            Task t = m.workingMethod();
+
+            Console.WriteLine("After Working method in Main");
+
+            t.ContinueWith((task, obj) => Console.WriteLine("Test"), TaskStatus.RanToCompletion);
+            Thread.Sleep(1000);
+        }
+
+        public async Task workingMethod()
+        {
+            Console.WriteLine("Working method.");
+
+            String result = await asyncMethod();
+
+            Console.WriteLine("Printing" +
+                              " result: " + result);
+        }
+
+        public async Task<String> asyncMethod()
+        {
+            await Task.Delay(100);
+            return "1234";
+        }
+    }
+}
