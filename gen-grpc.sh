8bd725212e7204cf9dc78412adc058386fc89589
Christopher Zell <Zelldon91@gmail.com>
2018-09-20 16:29:05 +0200

    add script to receive and generate proto

---
+++ gen-grpc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+protoPath=gateway-protocol/csharp-protocol
+gwProtoPath=${protoPath}/target/proto
+genPath=zb-grpc/proto
+
+cd ${protoPath}
+
+mvn clean install
+
+cd ../../
+
+~/.nuget/packages/grpc.tools/1.15.0/tools/linux_x64/protoc \
+  -I/usr/include/ \
+  -I${gwProtoPath} \
+  --csharp_out ${genPath} \
+  --grpc_out ${genPath} \
+  ${gwProtoPath}/gateway.proto \
+  --plugin="protoc-gen-grpc=/home/zell/.nuget/packages/grpc.tools/1.15.0/tools/linux_x64/grpc_csharp_plugin"
