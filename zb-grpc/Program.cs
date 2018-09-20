26e9203f265abb505cb3b27303e0047f1cd82625
Christopher Zell <Zelldon91@gmail.com>
2018-09-20 16:28:39 +0200

    add project files

---
+++ zb-grpc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GatewayProtocol;
+
+namespace zbgrpc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HealthRequest h = new HealthRequest();
+
+        }
+
+    }
+}
