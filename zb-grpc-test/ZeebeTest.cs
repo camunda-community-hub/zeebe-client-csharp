93d33e350235f6e552442b9d8b149b31e6e5719d
Christopher Zell <Zelldon91@gmail.com>
2018-11-02 09:05:45 +0100

    add test project

---
+++ zb-grpc-test/ZeebeTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using Zeebe.Impl;
+using GatewayProtocol;
+
+
+namespace zbgrpctest
+{
+    [TestFixture]
+    public class ZeebeTest
+    {
+        private readonly ZeebeClient zeebeClient = new ZeebeClient("localhost:26500");
+
+        [Test]
+        public async void RequestToplogy()
+        {
+
+            // given
+
+            // when
+            HealthResponse response = await zeebeClient.HealtRequest();
+
+            // then
+
+
+        }
+    }
+}
