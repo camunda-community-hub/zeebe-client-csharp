0a8c933526f8027ce75cf3b01123243f951bc214
Christopher Zell <Zelldon91@gmail.com>
2018-11-02 13:01:52 +0100

    create base class for zeebe tests

---
+++ zb-grpc-test/BaseZeebeTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Grpc.Core;
+using GatewayProtocol;
+using Zeebe.Impl;
+
+namespace zbgrpctest
+{
+    public class BaseZeebeTest
+    {
+        private Server server;
+        private GatewayTestService testService;
+        private ZeebeClient client;
+
+        public Server Server { get { return server; }}
+        public GatewayTestService TestService { get { return testService; }}
+        public ZeebeClient ZeebeClient { get { return client; }}
+
+
+        [SetUp]
+        public void Init()
+        {
+            client = new ZeebeClient("localhost:26500");
+            server = new Server();
+            server.Ports.Add(new ServerPort("localhost", 26500, ServerCredentials.Insecure));
+
+            testService = new GatewayTestService();
+            var serviceDefinition = Gateway.BindService(testService);
+            server.Services.Add(serviceDefinition);
+            server.Start();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            server.ShutdownAsync();
+        }
+    }
+}
---
+++ zb-grpc-test/RequestTopologyTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using Zeebe.Impl;
+using GatewayProtocol;
+using Grpc.Core.Testing;
+using Grpc.Core;
+
+namespace zbgrpctest
+{
+    [TestFixture]
+    public class RequestTopologyTest : BaseZeebeTest
+    {
+        [Test]
+        public async void ShouldSendRequestAsExpected()
+        {
+            // given
+            TopologyRequest expectedRequest = new TopologyRequest();
+
+            // when
+            TopologyResponse response = await ZeebeClient.TopologyRequest();
+
+            // then
+            var actualRequest = TestService.Requests[0];
+
+            Assert.AreEqual(expectedRequest, actualRequest);
+        }
+    }
+}
