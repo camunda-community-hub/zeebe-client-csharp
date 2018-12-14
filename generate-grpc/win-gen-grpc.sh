df0f69bf072ca872eb9a0a8b751b3707797392d0
Christopher Zell <Zelldon91@googlemail.com>
2018-12-14 19:33:04 +0100

    chore(gen): add generate script for windows

---
+++ generate-grpc/win-gen-grpc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+os=windows_x64
+grpcVersion=1.17.0
+zeebeVersion='0.14.0'
+protoFile=gateway.proto
+gwProtoPath=./
+genPath=Client/Impl/proto
+
+# go to root
+echo -e "cd ../\n"
+cd ../
+
+# restore packages
+echo -e "nuget restore Zeebe.sln\n"
+nuget restore Zeebe.sln
+
+# get gatway proto file
+echo -e "curl -X GET https://raw.githubusercontent.com/zeebe-io/zeebe/${zeebeVersion}/gateway-protocol/src/main/proto/gateway.proto > ${protoFile}\n"
+curl -X GET https://raw.githubusercontent.com/zeebe-io/zeebe/${zeebeVersion}/gateway-protocol/src/main/proto/${protoFile} > ${protoFile}
+
+
+# generate gRPC
+echo "
+ .packages/Grpc.Tools.${grpcVersion}/tools/${os}/protoc.exe \
+  -I/usr/include/ \
+  -I${gwProtoPath} \
+  --csharp_out ${genPath} \
+  --grpc_out ${genPath} \
+  ${gwProtoPath}/${protoFile} \
+  --plugin=\"protoc-gen-grpc=./packages/Grpc.Tools.${grpcVersion}/tools/${os}/grpc_csharp_plugin.exe\""
+echo -e "\n"
+
+./packages/Grpc.Tools.${grpcVersion}/tools/${os}/protoc.exe \
+  -I${gwProtoPath} \
+  --csharp_out ${genPath} \
+  --grpc_out ${genPath} \
+  ${gwProtoPath}/${protoFile} \
+  --plugin="protoc-gen-grpc=./packages/Grpc.Tools.${grpcVersion}/tools/${os}/grpc_csharp_plugin.exe"
+
+# clean up
+echo "rm ${protoFile}"
+rm ${protoFile}
