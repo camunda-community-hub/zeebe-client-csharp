0043ba18dbef43d787a58ed75bb8cca85b357061
Bernd Rücker <bernd.ruecker@camunda.com>
2019-01-31 18:31:10 +0100

    migrated test and example to .NET Core, including small refactorings to make project more C#-ish

--- Client.UnitTests/WorkflowResourceRequestTest.cs
+++ Client.UnitTests/WorkflowResourceRequestTest.cs
@@ -9,7 +9,7 @@ namespace Zeebe.Client
     [TestFixture]
     public class WorkflowResourceRequestTest : BaseZeebeTest
     {
-        private readonly string DemoProcessPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources/demo-process.bpmn");
+        private readonly string DemoProcessPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources","demo-process.bpmn");
 
         [Test]
         public async Task shouldSendRequestAsExpected()
