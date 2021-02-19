fbea6c0156ccd38cf7f33446cd4c3242c03fc027
xavier <xlegalles@gmail.com>
2021-02-19 17:35:02 +0100

    Variables extensions
    Variables extensions similar to the one present in the Java client.
    close #214

---
+++ Client.UnitTests/VariablesExtensionsTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Zeebe.Client.Impl.Responses;
+
+namespace Zeebe.Client
+{
+    public class VariablesExtensionsTest
+    {
+        [TestCaseSource(nameof(ValidVariables))]
+        public void ShouldDeserializeVariablesAsDictionary(string json, IDictionary<string, object> expected)
+        {
+            var variables = json.GetVariablesAsMap();
+            Assert.AreEqual(expected.Keys, variables.Keys);
+            Assert.AreEqual(expected.Values, variables.Values);
+        }
+
+        [TestCaseSource(nameof(InvalidVariables))]
+        public void ShouldNotDeserializeInvalidJson(string json)
+        {
+            Assert.Throws<JsonSerializationException>(() => json.GetVariablesAsMap());
+        }
+
+        [TestCaseSource(nameof(ValidVariables))]
+        public void ShouldDeserializeVariablesAsT(string json, IDictionary<string, object> _)
+        {
+            var variables = json.GetVariablesAsType<Dictionary<string, object>>();
+            Assert.IsInstanceOf<Dictionary<string, object>>(variables);
+        }
+
+        private static IEnumerable<object[]> ValidVariables
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    @"{'a' : 1, 'b' : 'any'}", new Dictionary<string, object> { { "a", 1 }, { "b", "any" } }
+                };
+                yield return new object[]
+                {
+                    @"{'a' : true, 'b' : { 'c' : 15 }}", new Dictionary<string, object> { { "a", true }, { "b", JObject.FromObject(new { c = 15 }) } }
+                };
+                yield return new object[]
+                {
+                    @"{'a' : null}", new Dictionary<string, object> { { "a", null } }
+                };
+                yield return new object[]
+                {
+                    @"{}", new Dictionary<string, object>()
+                };
+            }
+        }
+
+        private static IEnumerable<object[]> InvalidVariables
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    @"'a' : 1"
+                };
+                yield return new object[]
+                {
+                    @"['a' : true, 'b' 15 ]"
+                };
+            }
+        }
+    }
+}
---
+++ Client/Impl/Responses/VariableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Zeebe.Client.Impl.Responses
+{
+    /// <summary>
+    /// Defines some extensions to handle variables.
+    /// </summary>
+    public static class VariableExtensions
+    {
+        /// <summary>
+        /// Deserialize a JSON string as a dictionary of keys - the variable name - and their values.
+        /// Message variables must be a JSON object, as variables will be mapped in a
+        /// key-value fashion. e.g. { "a": 1, "b": 2 } will create two key/value pairs, named "a" and
+        /// "b" respectively, with their associated values. [{ "a": 1, "b": 2 }] would not be a
+        /// valid argument, as the root of the JSON document is an array and not an object.
+        /// </summary>
+        /// <param name="variables">Variables serialized as json.</param>
+        /// <param name="settings">Optional Json serializer settings.</param>
+        /// <returns>A deserialized dictionary of variables from the JSON string.</returns>
+        public static IReadOnlyDictionary<string, object> GetVariablesAsMap(this string variables, JsonSerializerSettings settings = null)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(variables, settings);
+        }
+
+        /// <summary>
+        /// Deserialize variables as T.
+        /// </summary>
+        /// <typeparam name="T">The target .NET type.</typeparam>
+        /// <param name="variables">Variables serialized as json.</param>
+        /// <param name="settings">Optional Json serializer settings.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static T GetVariablesAsType<T>(this string variables, JsonSerializerSettings settings = null) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(variables, settings);
+        }
+    }
+}
