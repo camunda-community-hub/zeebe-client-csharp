47e6dbbabbccc57f764b2aff0e8681fd744d0aca
Christopher Zell <zelldon91@googlemail.com>
2021-04-26 14:53:24 +0200

    feat: create and deploy script

---
+++ Client.Examples/createAndDeployImage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Expects that dotnet, docker and kubectl is installed and that a correct k8 context is set to deploy the example
+set -exuo pipefail
+
+## Builds the project
+cd ../
+dotnet restore *.sln
+dotnet build *.sln
+
+## Build image
+cd Client.Examples/
+docker build -t gcr.io/zeebe-io/zeebe-client-csharp-example:latest .
+
+## Publish image
+docker push gcr.io/zeebe-io/zeebe-client-csharp-example:latest
+
+## Deploy
+kubectl delete -f example.yaml
+kubectl apply -f example.yaml
+
+kubectl logs -l app=client-example -f
