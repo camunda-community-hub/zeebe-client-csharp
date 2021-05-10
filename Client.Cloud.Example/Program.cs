7f72ffb0bda21c17a2f2f0f2f02d3d158e85e8fb
Christopher Zell <zelldon91@googlemail.com>
2021-05-10 14:10:49 +0200

    feat: use FromEnv in Cloud example

--- Client.Cloud.Example/Program.cs
+++ Client.Cloud.Example/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using NLog.Extensions.Logging;
+using Zeebe.Client;
+using Zeebe.Client.Api.Builder;
 using Zeebe.Client.Impl.Builder;
 
 namespace Client.Cloud.Example
@@ -9,14 +11,10 @@ namespace Client.Cloud.Example
     {
         public static async Task Main(string[] args)
         {
-            var zeebeClient = CamundaCloudClientBuilder
-                .Builder()
-                .UseClientId(args[0])
-                .UseClientSecret(args[1])
-                .UseContactPoint(args[2])
-                .UseAuthServer("https://login.cloud.ultrawombat.com/oauth/token") // optional
-                .UseLoggerFactory(new NLogLoggerFactory()) // optional
-                .Build();
+            var zeebeClient =
+                    CamundaCloudClientBuilder.Builder().FromEnv()
+                    .UseLoggerFactory(new NLogLoggerFactory()) // optional
+                    .Build();
 
             var topology = await zeebeClient.TopologyRequest().Send();
 
