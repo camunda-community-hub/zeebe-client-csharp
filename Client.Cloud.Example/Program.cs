e4a465280a436f6b849505f5de9b6af34e2f2bcb
Christopher Zell <zelldon91@googlemail.com>
2021-05-10 12:32:38 +0200

    feat: Add camunda cloud example
    Add separate project as Camunda Cloud example, shows usage of
    CamundaCloudClient builder

---
+++ Client.Cloud.Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NLog.Extensions.Logging;
+using Zeebe.Client.Impl.Builder;
+
+namespace Client.Cloud.Example
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var zeebeClient = CamundaCloudClientBuilder
+                .Builder()
+                .UseClientId(args[0])
+                .UseClientSecret(args[1])
+                .UseContactPoint(args[2])
+                .UseAuthServer("https://login.cloud.ultrawombat.com/oauth/token") // optional
+                .UseLoggerFactory(new NLogLoggerFactory()) // optional
+                .Build();
+
+            var topology = await zeebeClient.TopologyRequest().Send();
+
+            Console.WriteLine("Hello: " + topology);
+        }
+    }
+}
