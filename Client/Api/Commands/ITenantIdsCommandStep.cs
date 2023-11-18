d594989a665f09484dce9ac63c35e4a22aba803b
ShawnAbshire <abshire.shawn@gmail.com>
2023-11-17 18:57:45 -0600

    feat(multitenancy): Reword ITenantIdsCommandStep method summaries.

--- Client/Api/Commands/ITenantIdsCommandStep.cs
+++ Client/Api/Commands/ITenantIdsCommandStep.cs
@@ -5,7 +5,7 @@ namespace Zeebe.Client.Api.Commands
     public interface ITenantIdsCommandStep<out T>
     {
         /// <summary>
-        /// Set a lit of tenantIds to associate to this resource.
+        /// Set a list of tenantIds to associate with this resource.
         /// </summary>
         /// <param name="tenantIds">the tenant to associate to this resource.</param>
         /// <returns>The builder for this command.  Call <see cref="IFinalCommandStep{T}.Send"/> to complete the command and send it
@@ -13,7 +13,7 @@ namespace Zeebe.Client.Api.Commands
         T TenantIds(IList<string> tenantIds);
 
         /// <summary>
-        /// Set a list of tenantIds to associate to this resource.
+        /// Set a list of tenantIds to associate with this resource.
         /// </summary>
         /// <param name="tenantIds">the tenant to associate to this resource.</param>
         /// <returns>The builder for this command.  Call <see cref="IFinalCommandStep{T}.Send"/> to complete the command and send it
