8ffaa88d84b862c9458b2ef016750cb77c5c5c42
Christopher Kujawa (Zell) <zelldon91@googlemail.com>
2024-02-01 15:25:01 +0100

    docs: enhance method doc

--- Client/Api/Commands/ITenantIdsCommandStep.cs
+++ Client/Api/Commands/ITenantIdsCommandStep.cs
@@ -21,7 +21,7 @@ namespace Zeebe.Client.Api.Commands
         /// 
         /// This can be useful when requesting jobs for multiple tenants at once. Each of the activated 
         /// jobs will be owned by the tenant that owns the corresponding process instance.
-        /// <param name="tenantIds">the tenant to associate to this resource.</param>
+        /// <param name="tenantIds">the identifiers of the tenants to specify for this command, e.g. ["ACME", "OTHER"]</param>
         /// <returns>The builder for this command.  Call <see cref="IFinalCommandStep{T}.Send"/> to complete the command and send it
         ///     to the broker.</returns>
         T TenantIds(params string[] tenantIds);
