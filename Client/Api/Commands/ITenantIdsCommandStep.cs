f78e3c693b04efd40689e11e96e3f5c06f92d9fb
Christopher Kujawa (Zell) <zelldon91@googlemail.com>
2024-02-01 15:24:30 +0100

    docs: enhnace method doc

--- Client/Api/Commands/ITenantIdsCommandStep.cs
+++ Client/Api/Commands/ITenantIdsCommandStep.cs
@@ -16,7 +16,7 @@ namespace Zeebe.Client.Api.Commands
         T TenantIds(IList<string> tenantIds);
 
         /// <summary>
-        /// Set a list of tenantIds to associate with this resource.
+        /// Specifies the tenants that may own any entities (e.g. process definition, process instances, etc.) resulting from this command.
         /// </summary>
         /// <param name="tenantIds">the tenant to associate to this resource.</param>
         /// <returns>The builder for this command.  Call <see cref="IFinalCommandStep{T}.Send"/> to complete the command and send it
