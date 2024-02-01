ed793b846d046600c15b374e5a749b3c8fa6eee6
Christopher Zell <zelldon91@googlemail.com>
2024-02-01 15:46:16 +0100

    build: update grpc

--- generate-grpc/gen-grpc.sh
+++ generate-grpc/gen-grpc.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 os=linux_x64
 grpcVersion=2.51.0
 packagePath=~/.nuget/packages/grpc.tools/${grpcVersion}/tools/${os}/
-zeebeVersion='8.3.0'
+zeebeVersion='8.4.1'
 protoFile=gateway.proto
 gwProtoPath=./
 genPath=Client/Impl/proto
